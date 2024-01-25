07e175471451b5449eb24a890b5aa1772f62922d
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-01-24 23:23:16 -0300

    Initial struct project

---
+++ guess-what-number/app.js
@@ -0,0 +1,11 @@
+alert("Hello, World");
+
+let SecretNumber = 29;
+
+let InputChoice = prompt("Escolha um número entre 1 e 30?");
+
+if (SecretNumber == InputChoice) {
+    alert("Você acertou!!");
+} else {
+    alert("Você errou");
+}
