60cf5109ec2bdc917cd7f86cd8a4c6c321da9e53
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-02-20 21:53:57 -0300

    Feature: Init styling project

--- Data-List/src/App.tsx
+++ Data-List/src/App.tsx
@@ -3,5 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 export function App() {
-    return <h1 className="text-3xl font-bold underline">hello world</h1>;
+    return (
+      <div className="py-10 space-y8">
+        <div>
+          header
+        </div>
+        <main className="max-w-6xl mx-auto space-y-5">
+
+        </main>
+
+      </div>
+    )
 }
