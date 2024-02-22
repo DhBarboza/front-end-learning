25acaaa15fbf971a288b30e87ce1cc46be292956
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-02-21 21:16:29 -0300

    Add components

--- Data-List/src/App.tsx
+++ Data-List/src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Plus } from "lucide-react";
 
 export function App() {
     return (
         <div className="py-10 space-y8">
             <div>header</div>
-            <main className="max-w-6xl mx-auto space-y-5"></main>
+            <main className="max-w-6xl mx-auto space-y-5">
+                <div className="flex items-center gap-4">
+                    <h1 className="text-xl font-bold">Tags</h1>
+                    <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font medium rounded-full p-2 hover:bg-teal-500 transition-colors">
+                        <Plus className="size-5" />
+                        Create new
+                    </button>
+                </div>
+            </main>
         </div>
     );
 }
