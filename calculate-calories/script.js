459e3241a6f5d874779fbcc3b8ebcea284bca76c
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-03-26 23:03:40 -0300

    feature: Add function targetInputContainer

--- calculate-calories/script.js
+++ calculate-calories/script.js
@@ -21,7 +21,25 @@ function cleanInputString(str) {
     }
 }*/
 
+// Remover caracteres de uma string str que correspondem a +, - ou espaços em branco.
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
     return str.replace(regex, "");
+}
+
+// verificando se a string str contém um padrão que corresponde a números no formato de notação científica, como por exemplo 1e10, 3E-5, etc:
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+// buscando por um elemento HTML com a classe .input-container dentro de um elemento identificado pelo valor selecionado em um dropdown:
+function addEntry() {
+    // Construindo um seletor CSS para encontrar um elemento HTML com o ID correspondente ao valor selecionado em um dropdown:
+    const targetId = "#" + entryDropdown.value;
+
+    // utiliza o método querySelector do objeto document para encontrar o primeiro elemento que corresponde ao seletor CSS construído anteriormente:
+    const targetInputContainer = document.querySelector(
+        `${targetId} .input-container`
+    );
 }
